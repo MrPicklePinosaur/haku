686134b987188c2bf73d298e9936ff9130d25b15
Wim Vanderbauwhede <Wim.Vanderbauwhede@mail.be>
2021-10-26 12:27:56 +0100

    shell script to run examples

---
+++ run-all-examples.sh
@@ -0,0 +1,13 @@
+./haku examples/adjectives.haku
+./haku examples/foldl.haku
+./haku examples/haku-raku.haku
+./haku examples/iroha.haku
+./haku examples/maps.haku
+./haku examples/mugendai.haku
+./haku examples/pwc131-t1.haku
+./haku examples/recursion_let.haku
+./haku examples/recursive_length_calc.haku
+./haku examples/yuki.haku
+./haku examples/zoi_chinamini.haku
+./haku examples/dailymaths-25.haku
+./haku examples/sum-of-list.haku
